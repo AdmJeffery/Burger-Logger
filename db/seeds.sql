0249d69694683ad7d1e1bac51c6f1a4f27bd6071
Ammon Jeffery <savingphace@gmail.com>
2020-08-17 23:19:11 -0600

    created 3 initial seeds for burger table

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Western Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Bleu Cheeseburger", false);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Atomic Fireball Burger", true);
