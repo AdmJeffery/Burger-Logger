928372629a364bdac49ad2ad2fd6a788bad443d7
Ammon Jeffery <savingphace@gmail.com>
2020-08-21 19:16:25 -0600

    completed burgers_controllers.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,4 +2,39 @@ let express = require("express");
 
 let router = express.Router();
 
-let burger = require("../models/burger.js")
+let burger = require("../models/burger.js");
+
+router.get("/", function(req, res){
+    burger.all(function(data){
+
+        res.render("index", {burger_data: data})
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function(result){
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0){
+            return res.status(404).end();
+        } else{
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router
