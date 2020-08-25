2233675af7685636bc9bfd23e413ae159d1032e4
Ammon Jeffery <savingphace@gmail.com>
2020-08-24 20:13:43 -0600

    global change in connection

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 //start making a connection to mysql
+var connection;
 
 var mysql = require("mysql");
 if (process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-var connection = mysql.createConnection({
+     connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
